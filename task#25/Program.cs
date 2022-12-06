f74a2707729fd4190ad2d08ed84157bebd435e38
DeadKn1ght <deadkn1ght@bk.ru>
2022-12-06 18:25:41 +0300

    Added task#27 and task #25 from Homework

---
+++ task#25/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Clear();
+
+Console.WriteLine("Input number A : ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Input number B : ");
+int numberB = int.Parse(Console.ReadLine());
+
+int Degree(int numberA, int numberB)
+{
+    int scale = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        scale *= numberA * ((i + 1) - i);
+    }
+    return scale;
+}
+int result = Degree(numberA, numberB);
+Console.WriteLine("Degree number B of number A is :");
+Console.WriteLine(result);
---
+++ task#27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+Console.WriteLine("Input number : ");
+int number = int.Parse(Console.ReadLine());
+
+int SummNumbers(int numb)
+{
+    int summnumb = 0;
+    for (summnumb = 0;numb > 0; numb = numb / 10)
+    {
+        summnumb = summnumb + (numb % 10);
+    }
+    return summnumb;
+}
+
+int summdigit = SummNumbers(number);
+Console.WriteLine($"Summ of all digits in number is : " + summdigit);
