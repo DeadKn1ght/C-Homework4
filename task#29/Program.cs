de12439459ae8f5c9037ae335c516de60ef48e29
DeadKn1ght <deadkn1ght@bk.ru>
2022-12-06 19:13:06 +0300

    Added task#29 from Homework

---
+++ task#29/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    for (int index = 0; index < length; index++)
+    {
+        collection[index] = new Random().Next(0, 100);
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for (int position = 0; position < count; position++)
+    {
+        Console.Write(" " + col[position]);
+    }
+}
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
+
